081234239dfaace6540d25e4f3ef7b4bcaf988ae
Richard Sanchez <richard.charles.s@gmail.com>
2018-06-18 23:32:35 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.match(/\A[A-Z]/) && text.match(/[.]\z/) 
+  if text.match(/\A[A-Z]/) && text.match(/[.]\z/)
     true
   else
     false
@@ -23,5 +23,8 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-
+  if phone.match(/\d{9})
+    true
+  else
+    false
 end
