4cdceec9d91066a18acfedd97f336e76bd6622b1
Richard Sanchez <richard.charles.s@gmail.com>
2018-06-18 23:41:17 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,5 @@
 def starts_with_a_vowel?(word)
-    if word.match(/\b[aeiou]/i)
-      true
-    else
-      false
-    end
+    word.match(/\b[aeiou]/i) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
@@ -15,11 +11,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.match(/\A[A-Z]/) && text.match(/[.]\z/)
-    true
-  else
-    false
-  end
+  text.match(/\A[A-Z]/) && text.match(/[.]\z/) ? true : false
 end
 
 def valid_phone_number?(phone)
