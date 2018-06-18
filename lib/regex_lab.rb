d6385c8881f2518c6930561c4b05daf0bbd95c8b
Richard Sanchez <richard.charles.s@gmail.com>
2018-06-18 22:46:05 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,7 +1,7 @@
 def starts_with_a_vowel?(word)
     if word.scan(/\b[aeiou]/i)
       true
-    else word.scan(/\[qwrtypsdfghjklzxcvbnm]/i)
+    else word.match(/\[qwrtypsdfghjklzxcvbnm]/i)
       false
     end
 end
