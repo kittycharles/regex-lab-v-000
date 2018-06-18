6252ad268dbcba7f68f8a87e015903d425538489
Richard Sanchez <richard.charles.s@gmail.com>
2018-06-18 23:14:40 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,9 @@
 def starts_with_a_vowel?(word)
-     word.match(/\b[aeiou]/i)
-    #   true
-    # else
-    #   false
-    # end
+    if word.match(/\b[aeiou]/i)
+      true
+    else
+      false
+    end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
